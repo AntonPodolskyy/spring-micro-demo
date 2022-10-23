ec68c4a20a7f165bb84189e503591fb188d33307
AntonPodolskyy <Allgamesxan1988>
2022-10-23 19:03:35 +0100

    Initial commit

---
+++ github-config-server/src/main/java/ru/javabegin/micro/demo/githubconfigserver/GithubConfigServerApplication.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.demo.githubconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubConfigServerApplication.class, args);
+    }
+
+}
